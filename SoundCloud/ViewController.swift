ee507019124339db68a321f1d9f8652699feaea2
grangegermi <dasha.voloshina@gmail.com>
2022-10-03 09:20:13 +0300

    Initial Commit

---
+++ SoundCloud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoundCloud
+//
+//  Created by Даша Волошина on 3.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
